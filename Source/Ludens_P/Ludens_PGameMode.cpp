fdc174cec21dcf0d9dab33e500de21e698f4f173
seong-ho-y <seonghoy88@gmail.com>
2025-03-31 22:52:28 +0900

    멀티 playerStart 위치 지정

--- Source/Ludens_P/Ludens_PGameMode.cpp
+++ Source/Ludens_P/Ludens_PGameMode.cpp
@@ -2,6 +2,9 @@
 
 #include "Ludens_PGameMode.h"
 #include "Ludens_PCharacter.h"
+#include "GameFramework/GameStateBase.h"
+#include "GameFramework/PlayerStart.h"
+#include "Kismet/GameplayStatics.h"
 #include "UObject/ConstructorHelpers.h"
 
 ALudens_PGameMode::ALudens_PGameMode()
@@ -11,4 +14,24 @@ ALudens_PGameMode::ALudens_PGameMode()
 	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPerson/Blueprints/BP_FirstPersonCharacter"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
+}
+// Ludens_PGameMode.cpp
+
+AActor* ALudens_PGameMode::ChoosePlayerStart_Implementation(AController* Player)
+{
+	TArray<AActor*> AllStarts;
+	UGameplayStatics::GetAllActorsOfClass(this, APlayerStart::StaticClass(), AllStarts);
+
+	for (AActor* Start : AllStarts)
+	{
+		if (!UsedStartSpots.Contains(Start))
+		{
+			UsedStartSpots.Add(Start);
+			UE_LOG(LogTemp, Warning, TEXT("Player assigned to Start: %s"), *Start->GetName());
+			return Start;
+		}
+	}
+
+	// 모두 사용됐으면 기본 처리
+	return Super::ChoosePlayerStart_Implementation(Player);
 }
--- Source/Ludens_P/Ludens_PGameMode.h
+++ Source/Ludens_P/Ludens_PGameMode.h
@@ -6,14 +6,22 @@
 #include "GameFramework/GameModeBase.h"
 #include "Ludens_PGameMode.generated.h"
 
+
+
 UCLASS(minimalapi)
 class ALudens_PGameMode : public AGameModeBase
 {
 	GENERATED_BODY()
 
+	// Ludens_PGameMode.h
+	UPROPERTY()
+	TArray<AActor*> UsedStartSpots;
+
+
+	virtual AActor* ChoosePlayerStart_Implementation(AController* Player) override; //ChoosePlayerStart
+
+
 public:
 	ALudens_PGameMode();
 };
-
-
 
