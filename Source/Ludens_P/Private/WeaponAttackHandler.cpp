37de24c317d20222d2b19c3f962e8908dd9457bd
Ko-K-Y <growch@naver.com>
2025-07-27 01:06:14 +0900

    플레이어 기능 구현
    PlayerStateComponent: 플레이어 상태에 대한 것을 담당
    PlayerAttackComponent: 플레이어 공격에 대한 것을 담당
    MeleeAttackHandler: 근접 공격 로직
    에임 UI 작업 완료
    Character Class 내: 더블 점프, 대쉬, 무기 재장전 및 공격 시 탄알 소모 기능 구현.
    추후 PlayerAttackComponent와 WeaponComponent를 연결할 예정
    개발 예정: 플레이어 기절 상태 서버 플레이어만 작동

---
+++ Source/Ludens_P/Private/WeaponAttackHandler.cpp
@@ -0,0 +1,67 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "WeaponAttackHandler.h"
+
+#include "CreatureCombatComponent.h"
+#include "GameFramework/Character.h"
+#include "Ludens_P/Ludens_PCharacter.h"
+
+
+void UWeaponAttackHandler::BeginPlay()
+{
+	
+}
+
+void UWeaponAttackHandler::HandleWeaponAttack(float damage)
+{
+	UE_LOG(LogTemp, Display, TEXT("HandleWeaponAttack!"));
+
+	if (!OwnerCharacter)
+	{
+		UE_LOG(LogTemp, Error, TEXT("OwnerCharacter is nullptr!"));
+		return;
+	}
+	// 무기 공격 로직 -> 공격 로직, 이펙트, 사운드 등등
+	
+	if (damage)
+	{
+		// 1. 플레이어 컨트롤러 얻기
+		APlayerController* PC = Cast<APlayerController>(OwnerCharacter->GetController());
+		if (!PC) return;
+
+		// 2. 화면 중앙 좌표 구하기
+		int32 ViewportX, ViewportY;
+		PC->GetViewportSize(ViewportX, ViewportY);
+		FVector2D ScreenCenter(ViewportX * 0.5f, ViewportY * 0.5f);
+
+		// 3. 화면 중심에서 월드 방향 구하기
+		FVector WorldLocation, WorldDirection;
+		if (!PC->DeprojectScreenPositionToWorld(ScreenCenter.X, ScreenCenter.Y, WorldLocation, WorldDirection))
+			return;
+
+		// 4. 트레이스 시작/끝 위치 계산
+		FVector TraceStart = WorldLocation;
+		FVector TraceEnd = TraceStart + (WorldDirection * WeaponRange);
+
+		// 5. 라인 트레이스
+		FHitResult Hit;
+		FCollisionQueryParams Params;
+		Params.AddIgnoredActor(OwnerCharacter);
+
+		// 라인 트레이스를 하여 무언가에 맞았는지를 나타냄
+		bool bHit = OwnerCharacter->GetWorld()->LineTraceSingleByChannel(Hit, TraceStart, TraceEnd, ECC_Pawn, Params);
+
+		// 6. CreatureCombatComponent가 있으면 데미지 적용
+		if (bHit && Hit.GetActor())
+		{
+			// 맞은 액터가 CCC를 가지고 있는지 검사
+			if (UCreatureCombatComponent* CombatComp = Hit.GetActor()->FindComponentByClass<UCreatureCombatComponent>())
+			{
+				CombatComp->TakeDamage(damage);
+			}
+		}
+	}
+}
+
+
