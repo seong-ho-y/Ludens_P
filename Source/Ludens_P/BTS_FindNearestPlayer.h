541b5d35ed0ef68141024f429d192f5d451a559b
seong-ho-y <seonghoy88@gmail.com>
2025-08-12 02:19:43 +0900

    BTT_Shoot & ServiceFNP
    Target을 일정시간마다 갱신하는 ServiceFNP 완성
    Shoot BTT 다시 리팩토링함 (3번째..)
    SCC랑 Fire연결해서 발사되는건 아직 안됨
    왜 안되는지 하...

---
+++ Source/Ludens_P/BTS_FindNearestPlayer.h
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTService.h"
+#include "BTS_FindNearestPlayer.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LUDENS_P_API UBTS_FindNearestPlayer : public UBTService
+{
+	GENERATED_BODY()
+public:
+	UBTS_FindNearestPlayer();
+
+protected:
+	UPROPERTY(EditAnywhere, Category="Blakcboard")
+	FBlackboardKeySelector TargetActorKey;
+
+	virtual void TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds) override;
+};
