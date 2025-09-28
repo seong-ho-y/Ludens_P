e690e84c9de1ec5fa068cdeede5b48abde22f150
hyoding <shj059184@gmail.com>
2025-09-29 02:55:43 +0900

    Lobby 관련 파일 추가
    Lobby 관련 파일 뼈대, 코드는 이 이후에 추가

---
+++ Source/Ludens_P/Private/LobbyPreviewRig.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "LobbyPreviewRig.h"
+
+// Sets default values
+ALobbyPreviewRig::ALobbyPreviewRig()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ALobbyPreviewRig::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ALobbyPreviewRig::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/Ludens_P/Public/WBP_Lobby.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "WBP_Lobby.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LUDENS_P_API UWBP_Lobby : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
