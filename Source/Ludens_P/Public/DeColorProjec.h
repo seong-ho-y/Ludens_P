0947bf1d810d58b0b77bb25e951e1a51f08726b6
Ko-K-Y <growch@naver.com>
2025-10-15 02:21:46 +0900

    Merge master to branch_kky

---
+++ Source/Ludens_P/Public/DeColorProjec.h
@@ -0,0 +1,51 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "DeColorProjec.generated.h"
+
+class USphereComponent;
+class UProjectileMovementComponent;
+class UNiagaraSystem;
+
+UCLASS()
+class LUDENS_P_API ADeColorProjec : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ADeColorProjec();
+
+	UPROPERTY(VisibleDefaultsOnly, Category = Projectile)
+	USphereComponent* CollisionComp;
+	
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	UStaticMeshComponent* MeshComp;
+
+	UPROPERTY(VisibleAnywhere, Category = Movement, meta = (AllowPrivateAccess = "true"))
+	UProjectileMovementComponent* ProjectileMovement;
+	
+	// 폭발 시 피해량과 범위를 설정할 변수
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Damage")
+	float DamageAmount = 9999.0f;
+
+	UPROPERTY(EditAnywhere, Category = "VFX")
+	UNiagaraSystem* ExploVFX;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UFUNCTION()
+	void OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse,
+			   const FHitResult& Hit);
+	UFUNCTION(NetMulticast, Reliable)
+	void SpawnExploVFX();
+};
