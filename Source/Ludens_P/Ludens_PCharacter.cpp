b3683c89dc6be1f91cab7a8b749f6cf7fc810b07
Ko-K-Y <growch@naver.com>
2025-11-12 21:33:04 +0900

    Reload 로직 수정
    2초 뒤에 재장전 되도록

--- Source/Ludens_P/Ludens_PCharacter.cpp
+++ Source/Ludens_P/Ludens_PCharacter.cpp
@@ -546,6 +546,7 @@ void ALudens_PCharacter::Fire(const FInputActionValue& Value)
 		Server_Fire(Value);
 		return;
 	}
+	UE_LOG(LogTemp, Warning, TEXT(""));
 	if (PlayerStateComponent->IsDead || PlayerStateComponent->IsKnocked || WeaponComponent->bIsWeaponAttacking || bIsReloading) return;
 	if (CurrentAmmo > 0)
 	{
@@ -581,12 +582,18 @@ void ALudens_PCharacter::Reload(const FInputActionValue& Value)
 void ALudens_PCharacter::HandleReload()
 {
 	if (bIsReloading) return; // 이미 재장전 중이면 리턴
-	
+
+	GetWorld()->GetTimerManager().SetTimer(ReloadTimerHandle, this, &ALudens_PCharacter::EndReload, ReloadTime, false);
+}
+
+void ALudens_PCharacter::EndReload()
+{
 	bIsReloading = true;
 	if (CurrentAmmo != MaxAmmo)
 	{
 		if (SavedAmmo <= 0)
 		{
+			bIsReloading = false;
 			return;
 		}
 		else if (SavedAmmo - (MaxAmmo - CurrentAmmo) <= 0)
@@ -600,12 +607,6 @@ void ALudens_PCharacter::HandleReload()
 			CurrentAmmo = MaxAmmo;
 		}
 	}
-
-	GetWorld()->GetTimerManager().SetTimer(ReloadTimerHandle, this, &ALudens_PCharacter::EndReload, ReloadTime, false);
-}
-
-void ALudens_PCharacter::EndReload()
-{
 	bIsReloading = false;
 }
 
